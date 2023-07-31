b1a2bee8b7449bb92eef17a61d07a4b58dc897e8
Zach Stucky <zkstucky@gmail.com>
2023-07-31 09:02:16 -0500

    Added Core Data, AppDelegate, SceneDelegate files

---
+++ Transactions+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Transactions+CoreDataClass.swift
+//  Budget
+//
+//  Created by Zach Stucky on 7/31/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Transactions)
+public class Transactions: NSManagedObject {
+
+}
